33c3e2b114111c7b7732dec142376430d501d4c1
Ibrahim Ahmed <ibrahim3hmedib@gmail.com>
2024-03-28 06:32:40 +0200

    Refactor authorization middleware to handle unauthorized and forbidden access

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,19 +1,20 @@
 import rbac from "../modules/rbac/rbac.js";
+import { appError } from "./errorHandler.js";
 
 // Authorization middleware
 export default (action, resource) => async (req, res, next) => {
   try {
     const { user } = req;
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-    
+    if (!user || !user.isVerified)
+      return next(new appError("Unauthorized", 401));
+
+    // Check if the user has permission to perform the action on the resource
     const allowed = await rbac.can(user.role, action, resource);
-    if (!allowed) {
-      return res.status(403).json({ message: "Forbidden" });
-    }
+    if (!allowed) return next(new appError("Forbidden, limited access.", 403));
+
+    // If the user has permission, continue to the next middleware
     next();
   } catch (err) {
-    next(err);
+    next(new appError(err.message, 500));
   }
 };
